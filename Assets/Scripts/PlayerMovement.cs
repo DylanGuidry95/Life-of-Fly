c096e33d51ebcdae8516c9e18b5cc5e4e8c93623
Eric <EricMouledoux@Gmail.com>
2016-10-14 21:53:56 -0500

    Flying, but still spinning around z

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,10 +16,30 @@ public class PlayerMovement : MonoBehaviour
         mVelocity.z = Input.GetAxis("Vertical");
         mVelocity.x = Input.GetAxis("Horizontal");
 
+        mVelocity.y = Input.GetAxis("Fly");
+        isFlying = mVelocity.y != 0;
+
+        mDirection = Vector3.zero;
+
+        mDirection.x = Input.GetAxis("LookVertical");
+        mDirection.y = Input.GetAxis("LookHorizontal");
+
+        mVelocity *= Time.deltaTime;
+        //mDirection *= Time.deltaTime;
+
         transform.Translate(mVelocity, Space.Self);
+        transform.Rotate(mDirection, Space.Self);
+        
+        if(transform.localEulerAngles.z != 0 && isFlying)
+        {
+            transform.Rotate(0, 0, -transform.localEulerAngles.z * Time.deltaTime);
+        }
     }
 
     private Rigidbody rb;
     private Vector3 mVelocity;
+    private Vector3 mDirection;
     private float mMaxSpeed;
+
+    private bool isFlying;
 }
