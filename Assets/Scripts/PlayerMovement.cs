afa8e031e1e27447c6913f630220145d6979ddb4
Eric <EricMouledoux@Gmail.com>
2016-10-14 20:33:26 -0500

    Player moves, but only in global directions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-	// Use this for initialization
-	void Start () {
-	
+	void Update ()
+    {
+        mVelocity = Vector3.zero;
+
+        mVelocity.z = Input.GetAxis("Vertical");
+        mVelocity.x = Input.GetAxis("Horizontal");
+
+        transform.position += mVelocity;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private Rigidbody rb;
+    private Vector3 mVelocity;
+    private float mMaxSpeed;
 }
