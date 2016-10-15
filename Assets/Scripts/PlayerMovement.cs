72bbe7881d00da71afeaa2c509a28c24b134e4b8
Eric <EricMouledoux@Gmail.com>
2016-10-14 23:51:35 -0500

    All movement delta time based

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,18 +15,24 @@ public class PlayerMovement : MonoBehaviour
 
         mVelocity.z = Input.GetAxis("Vertical");
         mVelocity.x = Input.GetAxis("Horizontal");
-        rb.AddRelativeForce(mVelocity, ForceMode.Acceleration);
+        rb.AddRelativeForce(mVelocity * Time.deltaTime, ForceMode.Acceleration);
 
         mVelocity = Vector3.zero;
 
         mVelocity.y = Input.GetAxis("Fly");
-        rb.AddRelativeForce(mVelocity, ForceMode.Acceleration);
+        rb.AddRelativeForce(mVelocity * Time.deltaTime, ForceMode.Acceleration);
 
         mDirection = Vector3.zero;
 
         mDirection.x = Input.GetAxis("LookVertical");
         mDirection.y = Input.GetAxis("LookHorizontal");
-        rb.AddTorque(mDirection, ForceMode.Acceleration);
+        rb.AddRelativeTorque(mDirection * Time.deltaTime, ForceMode.Acceleration);
+
+        if(isFlying)
+        {
+            float zDif = -1 * transform.localEulerAngles.z;
+            rb.AddRelativeTorque(0, 0, zDif * Time.deltaTime, ForceMode.Acceleration);
+        }
     }
 
     private Rigidbody rb;
